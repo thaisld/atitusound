e70f77c2aa547476a2d02494a6b32e22bb5f9eba
Thaís Lodi Pinheiro <137234283+thaisld@users.noreply.github.com>
2023-11-21 09:46:22 -0300

    Update PlaylistRepository.java

--- atitusound/src/main/java/br/atitus/poo/atitusound/repository/PlaylistRepository.java
+++ atitusound/src/main/java/br/atitus/poo/atitusound/repository/PlaylistRepository.java
@@ -1,4 +1,4 @@
-package br.atitus.poo.atitusound.repositories;
+package br.edu.atitus.poo.atitusound.repositories;
 
 import java.util.List;
 
@@ -6,8 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import br.atitus.poo.atitusound.entities.PlaylistEntity;
-import br.atitus.poo.atitusound.entities.UserEntity;
+import br.edu.atitus.poo.atitusound.entities.PlaylistEntity;
+import br.edu.atitus.poo.atitusound.entities.UserEntity;
 
 @Repository
 public interface PlaylistRepository extends GenericRepository<PlaylistEntity>{
