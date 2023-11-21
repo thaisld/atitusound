a3fb1664dc39767fa7ba3fc0a9fe24f85f2ee31e
Thaís Lodi Pinheiro <137234283+thaisld@users.noreply.github.com>
2023-11-21 09:50:30 -0300

    Update AtitusoundApplication.java

--- atitusound/src/main/java/br/atitus/poo/atitusound/AtitusoundApplication.java
+++ atitusound/src/main/java/br/atitus/poo/atitusound/AtitusoundApplication.java
@@ -1,4 +1,4 @@
-package br.atitus.poo.atitusound;
+package br.edu.atitus.poo.atitusound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
